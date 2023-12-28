bec584bbfdaafec37d594f8b4e9492d561762cb5
ATHARV DALAL <sangamdalal24@gmail.com>
2023-12-28 22:02:25 +0530

    Fixing Some Errors

---
+++ src/app/login/page.js
@@ -0,0 +1,8 @@
+export default function login(){
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <h1>
+                i am login
+            </h1>
+            </div>
+    )
