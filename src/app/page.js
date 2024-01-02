88b9c27cd826e9ae9a8557687b6357db358b7a4f
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-02 14:12:54 +0530

    Fetching API in Next Js

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,43 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import './style.css'
+
 
 export default function Home() {
 
+  const [product, setProduct]= useState([])
+
+  useEffect(()=>{
+      const FetchData = async ()=>{
+           const res = await fetch("https://dummyjson.com/products?limits=10")
+           const data = await res.json()
+           setProduct(data.products)
+           console.log(data);
+           
+
+      }
+      
+
+
+      FetchData()
+  },[])
+
   
   return (
-    <main className='hero'>
+   <div>
+
+   {
+    product.map((item)=>{
+      return( 
+      <div key={item.id}>
+      <h1> {item.title} </h1>
+      <img src={item.thumbnail} alt="" />
+      </div>
       
-     hello
-     
-
-
-    </main>
+      )
+    })
+   }
+   </div>
   )
 }
